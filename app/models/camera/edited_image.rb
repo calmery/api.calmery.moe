cb377ac998a23470a1d8fe8deaa19fcaa01139dd
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-09-15 21:48:48 +0900

    開発環境では public フォルダ内に画像を保存するように変更する

--- app/models/camera/edited_image.rb
+++ app/models/camera/edited_image.rb
@@ -6,11 +6,19 @@ class Camera::EditedImage < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   def image_url
-    Cloudinary::Utils.cloudinary_url image.public_id
+    if Rails.env.production?
+      return Cloudinary::Utils.cloudinary_url(image.public_id)
+    end
+
+    image.url
   end
 
   def og_image_url
-    Cloudinary::Utils.cloudinary_url(image.public_id, :background => '#fff', :crop => :pad, :height => 630, :width => 1200)
+    if Rails.env.production?
+      return Cloudinary::Utils.cloudinary_url(image.public_id, :background => '#fff', :crop => :pad, :height => 630, :width => 1200)
+    end
+
+    image.url
   end
 
   private
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
 class ImageUploader < CarrierWave::Uploader::Base
-  include Cloudinary::CarrierWave
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
 
-  def public_id
-    "#{model.model_name.collection}/#{model.id}"
+    def public_id
+      "#{model.model_name.collection}/#{model.id}"
+    end
+  else
+    storage :file
+
+    def asset_host
+      return "http://localhost:5000"
+    end
+
+    def filename
+      "#{model.id}.#{file.extension}"
+    end
+
+    def store_dir
+      "uploads/#{model.model_name.collection}"
+    end
   end
 end
