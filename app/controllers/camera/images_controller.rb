cc8fce190f169f173378d9d2d150e64b8582db9d
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-09-15 16:53:14 +0900

    Cloudinary に画像をアップロードできるようにする

--- app/controllers/camera/images_controller.rb
+++ app/controllers/camera/images_controller.rb
@@ -3,5 +3,9 @@
 class Camera::ImagesController < Camera::ApplicationController
   def show; end
 
-  def create; end
+  def create
+    camera_edited_image = Camera::EditedImage.new
+    camera_edited_image.image = params[:image]
+    camera_edited_image.save!
+  end
 end
