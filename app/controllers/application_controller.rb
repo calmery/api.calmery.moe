f1cc6fee1e7df360d54ce9efde6ffc91773d30aa
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-09-15 17:32:26 +0900

    パラメータに画像が含まれないときは 400 を返す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def status(http_status_code)
+    render status: http_status_code, body: nil
+  end
 end
--- app/controllers/camera/images_controller.rb
+++ app/controllers/camera/images_controller.rb
@@ -4,8 +4,12 @@ class Camera::ImagesController < Camera::ApplicationController
   def show; end
 
   def create
+    image = params[:image]
+
+    return status 400 if image.nil?
+
     camera_edited_image = Camera::EditedImage.new
-    camera_edited_image.image = params[:image]
+    camera_edited_image.image = image
     camera_edited_image.save!
   end
 end
