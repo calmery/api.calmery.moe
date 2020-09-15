0e46355d33401735da5f58b0d14da3809f7c4f5a
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-09-15 18:31:52 +0900

    RuboCop に従って修正する

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,7 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'localhost:3000', '127.0.0.1:3000', 'camera.calmery.moe'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
