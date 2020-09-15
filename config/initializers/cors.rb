83e1a24536f2baf90001c267f36ab09ac9280667
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-09-15 23:37:34 +0900

    develop.camera.calmery.moe からのアクセスを許可する

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', '127.0.0.1:3000', 'camera.calmery.moe'
+    origins 'localhost:3000', '127.0.0.1:3000', 'camera.calmery.moe', 'develop.camera.calmery.moe'
 
     resource '*',
              headers: :any,
