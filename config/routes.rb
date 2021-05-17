b9378785de0e234b8b07a54fa9fabd896893b284
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-05-18 03:31:32 +0900

    Heroku を Free プランにしたため、カメラへのアクセス時にリクエストを投げてサーバを起動できるようにする

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: 'root#index'
+
   namespace :camera do
     resources :images, only: %i[show create]
     resources :showcase, only: %i[index show], param: :page
